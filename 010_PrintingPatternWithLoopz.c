4a5d3a1a73b3953e3f289969ced70be2b407e731
dang0x5f <dannypgehrz@gmail.com>
2024-08-30 13:25:30 -0500

    Committing to work on other computer., nothing done.

---
+++ 010_PrintingPatternWithLoopz.c
@@ -0,0 +1,26 @@
+/* height = layers + border_num */
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void print_pattern(int, int);
+
+int main(void){
+
+    int n, wxh;
+    scanf("%d", &n);
+
+    wxh = (n << 1) - 1;
+
+
+
+    return(0);
+}
+
+void print_pattern(int o_layer, int wxh){
+    int step = 0;
+    int curr = 0;
+    
+}
