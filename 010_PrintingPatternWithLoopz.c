30c152c66d6c623aa6e0d887bb88182d3a1dfca7
dang0x5f <dannypgehrz@gmail.com>
2024-09-02 16:05:02 -0500

    Sync;

--- 010_PrintingPatternWithLoopz.c
+++ 010_PrintingPatternWithLoopz.c
@@ -3,9 +3,12 @@
 #include <math.h>
 #include <stdlib.h>
 
+#define ASCEND_FLAG  (1<<7)
+#define DESCEND_FLAG (1<<6)
+
 void print_pattern(int*);
 void print_row(int, int, int);
-void print_col(int, int);
+void print_col(int, int, int);
 
 int main(void){
     int n;
@@ -20,19 +23,28 @@ int main(void){
 void print_pattern(int* num){
     int wxh;
     wxh = (*num << 1) - 1;
-    print_row(wxh, *num, 1);
+    print_row(wxh, *num, 0);
 }
 
 void print_row(int wxh, int o_layer, int row){
-    print_col(wxh, o_layer);
-
-    if(row <= wxh)
+    if(row < wxh){
+        print_col(wxh, o_layer, row);
         print_row(wxh, o_layer, row + 1);
+    }
 }
 
-void print_col(int wxh, int o_layer){
-    for(int i = 0; i < wxh; i++)
-        printf("%d  ", o_layer);
+void print_col(int wxh, int o_layer, int row){
+    for(int i = 0; i < wxh; i++){
+        /* printf("%d ", o_layer); */
+
+        /* middle */
+        if( i == (wxh/2) && row == (wxh/2) ){
+            printf("%c ", '+');
+        } else {
+            printf("%d ", o_layer);
+        }
+
+    }
 
     printf("\n");
 }
