0ffd4cf1772015498e4de78c192a3dc8ae12c452
dang0x5f <dannypgehrz@gmail.com>
2024-09-03 17:31:29 -0500

    Took longer than I'd like to admit, but #010 solved.

--- 010_PrintingPatternWithLoopz.c
+++ 010_PrintingPatternWithLoopz.c
@@ -3,12 +3,9 @@
 #include <math.h>
 #include <stdlib.h>
 
-#define ASCEND_FLAG  (1<<7)
-#define DESCEND_FLAG (1<<6)
-
 void print_pattern(int*);
-void print_row(int, int, int);
-void print_col(int, int, int);
+void print_row(int, int, int, int);
+void print_col(int, int, int, int);
 
 int main(void){
     int n;
@@ -23,28 +20,39 @@ int main(void){
 void print_pattern(int* num){
     int wxh;
     wxh = (*num << 1) - 1;
-    print_row(wxh, *num, 0);
+    print_row(wxh, *num, 0, 0);
 }
 
-void print_row(int wxh, int o_layer, int row){
-    if(row < wxh){
-        print_col(wxh, o_layer, row);
-        print_row(wxh, o_layer, row + 1);
+void print_row(int wxh, int o_layer, int row, int step){
+    if(row < wxh && row < (wxh/2) ){
+        print_col(wxh, o_layer, row, step);
+        print_row(wxh, o_layer, row + 1, step + 1);
+    } else if(row < wxh && row >= (wxh/2) ){
+        print_col(wxh, o_layer, row, step);
+        print_row(wxh, o_layer, row + 1, step - 1);
     }
 }
 
-void print_col(int wxh, int o_layer, int row){
+/* inc_step is the accumulator */ 
+/* step is the max amount of steps down/up */
+void print_col(int wxh, int o_layer, int row, int step){
+
+    int inc_step = 0;
+
     for(int i = 0; i < wxh; i++){
-        /* printf("%d ", o_layer); */
-
         /* middle */
-        if( i == (wxh/2) && row == (wxh/2) ){
-            printf("%c ", '+');
-        } else {
-            printf("%d ", o_layer);
-        }
+        /* if( i == (wxh/2) && row == (wxh/2) ){ */
+        /*     printf("%c ", '+'); */
+        /* } else { */
+        /*     printf("%d ", o_layer); */
+        /* } */
 
+        printf("%d ", o_layer - inc_step);
+
+        if(i < (wxh/2) && inc_step < step)
+            inc_step++;
+        else if(i >= (wxh/2) && inc_step > 0 && i >= (wxh - step - 1))
+            inc_step--;
     }
-
     printf("\n");
 }
