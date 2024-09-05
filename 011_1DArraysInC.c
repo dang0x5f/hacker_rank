c3dbc975da80a7690ba5b834b224197bbeadd95c
dang0x5f <dannypgehrz@gmail.com>
2024-09-04 22:02:34 -0500

    sync. dvorak => 62

--- 011_1DArraysInC.c
+++ 011_1DArraysInC.c
@@ -2,5 +2,20 @@
 #include <stdlib.h>
 
 int main(void){
+    int sum = 0, n, *np;
+    scanf("%d", &n);
+
+    np = calloc(n, sizeof(int));
+
+    for(int i = 0; i < n; i++)
+       scanf("%d", np + i); 
+
+    for(int i = 0; i < n; i++)
+        sum += *(np + i);
+
+    printf("%d\n", sum);
+
+    free(np);
+    
     return(0);
 }
